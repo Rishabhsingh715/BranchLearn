8e92bb0f869b2452cb5b617443c9d4465c5369d6
Rishabh singh <srishabh715@gmail.com>
2021-07-01 12:01:01 +0530

    added new print implementation

--- Diff.java
+++ Diff.java
@@ -1,9 +1,14 @@
 class Diff{
 
+	static void print(){
+              System.out.println("This is new implementation for print now");
+
+	}
+
 public static void main(String args[]){
 
 System.out.println("This is multiply code");
-
+ print();
 
 }
 
